8afd49fffb5fa89afb21ce304f692f18a6d7b426
VlogBox Dev Team <development@vlogbox.com>
2019-03-27 19:47:18 +0200

    bif file factory interface fix

--- src/Factory/BifFileFactory.php
+++ src/Factory/BifFileFactory.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace BifFileProducer\Factory;
 
-use BifGenerator\Factory\BifFileFactoryInterface;
 use BifGenerator\FrameProvider\FrameCollection;
 
 /**
--- src/Factory/BifFileFactoryInterface.php
+++ src/Factory/BifFileFactoryInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace BifGenerator\Factory;
+namespace BifFileProducer\Factory;
 
 use BifGenerator\FrameProvider\FrameCollection;
 
